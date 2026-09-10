01cee9bb83b155eae9d66285a8b6ad3da00732f0
davinairs <davinairsanti@gmail.com>
2026-09-10 16:45:06 +0700

    updated

--- src/components/CompanyModal.jsx
+++ src/components/CompanyModal.jsx
@@ -18,7 +18,6 @@ function CompanyModal({
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            onClick={onClose}
             className="absolute inset-0 bg-black/40 backdrop-blur-2xs"
           />
           <motion.div
--- src/components/EquipmentModal.jsx
+++ src/components/EquipmentModal.jsx
@@ -55,8 +55,7 @@ function EquipmentModal({
     : "Available";
 
   return (
-    <div onClick={onClose}
-    className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 p-4">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 p-4">
       <motion.div
         initial={{ opacity: 0, scale: 0.95 }}
         animate={{ opacity: 1, scale: 1 }}
